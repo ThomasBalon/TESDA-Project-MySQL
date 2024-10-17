06a54e01c6b0d8c6ce360fbdc55e20b6f827515e
Dave Andrew Solayao <102861448+code-solayao@users.noreply.github.com>
2024-10-17 16:18:44 +0800

    update 10/17/24

--- tesda_project.sql
+++ tesda_project.sql
@@ -37,5 +37,22 @@ END//
 DELIMITER ;
 */
 
+/* DELIMITER //
+CREATE PROCEDURE update_data(IN _id INT, IN _lastname VARCHAR(50), IN _firstname VARCHAR(50), IN _middleinitial CHAR(10), IN _suffix CHAR(10), IN _sex VARCHAR(50), IN _birthdate DATE, 
+								IN _address VARCHAR(255), IN _qualification VARCHAR(255), IN _tviname VARCHAR(255), IN _district VARCHAR(255), IN _city VARCHAR(255), 
+                                IN _scholarshiptype VARCHAR(255), IN _graduationyear INT)
+BEGIN
+	UPDATE scholarship_employment 
+    SET last_name = _lastname, first_name = _firstname, middle_initial = _middleinitial, suffix = _suffix, sex = _sex, birthdate = _birthdate, address = _address, 
+		qualification = _qualification, tvi_name = _tviname, district = _district, city = _city, scholarship_type = _scholarshiptype, graduation_year = _graduationyear
+	WHERE id = _id;
+END//
+DELIMITER ;
+*/
+
+-- CALL update_data(1, "Solayao", "Dave Andrew", "A.", "", "Male", "2001-07-03", "Blk. 21 Lot 15, Berkeley Heights Subd., Brgy. Pulong Sta. Cruz, Sta. Rosa City, Laguna", "BSIT", "STI College Sta. Rosa", "N/A", "Santa Rosa", "PWSP", 2024);
+
 -- TRUNCATE TABLE scholarship_employment;
 -- DROP TABLE scholarship_employment;
+
+-- SET AUTOCOMMIT = ON;
