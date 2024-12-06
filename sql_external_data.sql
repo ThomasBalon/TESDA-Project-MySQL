54126adcbb44fe6695d053feceb909522a045d23
Dave Andrew Solayao <102861448+code-solayao@users.noreply.github.com>
2024-12-06 15:50:15 +0800

    update 12/06/24

--- sql_external_data.sql
+++ sql_external_data.sql
@@ -8,9 +8,9 @@
     employment_type VARCHAR(255) NULL, 
     date_hired VARCHAR(50) NULL, 
     remarks VARCHAR(255) NULL, 
-    count INT NULL, 
-    no_of_graduates INT NULL, 
-    no_of_employed INT NULL, 
+    count CHAR(10) NULL, 
+    no_of_graduates CHAR(10) NULL, 
+    no_of_employed CHAR(10) NULL, 
     verification VARCHAR(50) NULL, 
     job_vacancies CHAR(10) NULL, 
     FOREIGN KEY(Id) REFERENCES initial_records(Id)
@@ -23,7 +23,47 @@ DELIMITER //
 CREATE PROCEDURE initialise_external_record (IN Id INT) 
 BEGIN
 	INSERT INTO external_records 
-    VALUES (_Id, "", "", "", "", "", "", "", "", null, null, null, "", "");
+    VALUES (_Id, "", "", "", "", "", "", "", "", "", "", "", "", "");
+END//
+DELIMITER ;
+*/
+
+/*
+DELIMITER //
+CREATE PROCEDURE exceldata_import(IN _Id INT, IN _verifmeans VARCHAR(50), IN _verifdate VARCHAR(50), 
+								IN _verifstatus VARCHAR(50), IN _followupdate_1 VARCHAR(50), IN _followupdate_2 VARCHAR(50), IN _response VARCHAR(50), 
+                                IN _notinterested VARCHAR(255), IN _refstatus CHAR(10), IN _refdate VARCHAR(50), IN _norefreason VARCHAR(255), 
+                                IN _invalidcontact CHAR(10), IN _companyname VARCHAR(255), IN _companyaddress VARCHAR(255), IN _jobtitle VARCHAR(255), 
+                                IN _empstatus VARCHAR(255), IN _hireddate VARCHAR(50), IN _submitdocsdate VARCHAR(50), IN _interviewdate VARCHAR(50), 
+                                IN _nothiredreason VARCHAR(50), IN _training VARCHAR(50), IN _assessment VARCHAR(255), IN _empbeforetraining VARCHAR(50), 
+                                IN _occupation VARCHAR(255), IN _employer VARCHAR(255), IN _emptype VARCHAR(255), IN _datehired VARCHAR(50), 
+                                IN _remarks VARCHAR(255), IN _count CHAR(10), IN _graduates CHAR(10), IN _employed CHAR(10), 
+                                IN _verification VARCHAR(50), IN _jobvacancies CHAR(10))
+BEGIN
+	INSERT INTO verification_records 
+    VALUES (_Id, _verifmeans, _verifdate, _verifstatus, _followupdate_1, _followupdate_2, _response, _notinterested, _refstatus, _refdate, _norefreason, 
+			_invalidcontact);
+    INSERT INTO employment_records 
+    VALUES (_Id, _companyname, _companyaddress, _jobtitle, _empstatus, _hireddate, _submitdocsdate, _interviewdate, _nothiredreason);
+    INSERT INTO external_records 
+    VALUES (_Id, _training, _assessment, _empbeforetraining, _occupation, _employer, _emptype, _datehired, _remarks, _count, _graduates, _employed, 
+			_verification, _jobvacancies);
+END//
+DELIMITER ;
+*/
+
+/*
+DELIMITER //
+CREATE PROCEDURE exceldata_readall()
+BEGIN
+	SELECT initial_records.Id, district, city, tvi, qualification_title, sector, last_name, first_name, middle_name, extension_name, full_name, 
+			contact_number, email, scholarship_type, training_status, assessment_result, employment_before_training, occupation, employer_name, 
+            employment_type, address, date_hired, allocation, verification_means, verification_date, verification_status, follow_up_date_1, 
+            response_status, not_interested_reason, referral_status, company_name, company_address, job_title, employment_status, hired_date, remarks, 
+            count, no_of_graduates, no_of_employed, verification, job_vacancies 
+    FROM ((initial_records LEFT JOIN verification_records ON initial_records.Id = verification_records.Id) 
+    LEFT JOIN employment_records ON initial_records.Id = employment_records.Id) 
+    LEFT JOIN external_records ON initial_records.Id = external_records.Id;
 END//
 DELIMITER ;
 */
--- tesda_project.sql
+++ tesda_project.sql
@@ -54,44 +54,6 @@ DELETE FROM employment_records WHERE id = 1;
 
 /*
 DELIMITER //
-CREATE PROCEDURE exceldata_import(IN _district VARCHAR(50), IN _city VARCHAR(50), IN _tvi VARCHAR(255), IN _qualification VARCHAR(255), IN _sector VARCHAR(255), IN _lastname VARCHAR(255), 
-							IN _firstname VARCHAR(255), IN _middlename VARCHAR(255), IN _extnname VARCHAR(50), IN _fullname VARCHAR(255), IN _contactnum VARCHAR(16), IN _email VARCHAR(255), 
-                            IN _scholarship VARCHAR(50), IN _training VARCHAR(50), IN _assessment VARCHAR(50), IN _emp_before_training VARCHAR(50), IN _occupation VARCHAR(255), 
-                            IN _employer VARCHAR(255), IN _emptype VARCHAR(255), IN _address VARCHAR(255), IN _datehired VARCHAR(50), IN _allocation VARCHAR(50), 
-                            IN _verifmeans VARCHAR(50), IN _verifdate VARCHAR(50), IN _verifstatus VARCHAR(50), IN _followupdate VARCHAR(50), IN _response VARCHAR(50), 
-                            IN _notinterested VARCHAR(255), IN _refstatus CHAR(10), IN _companyname VARCHAR(255), IN _companyaddress VARCHAR(255), IN _jobtitle VARCHAR(255), 
-                            IN _empstatus VARCHAR(255), IN _hireddate VARCHAR(50))
-BEGIN
-	INSERT INTO initial_records (district, city, tvi, qualification_title, sector, last_name, first_name, middle_name, extension_name, full_name, contact_number, email, 
-								scholarship_type, training_status, assessment_result, employment_before_training, occupation, employer_name, employment_type, address, date_hired, 
-                                allocation) 
-	VALUES (_district, _city, _tvi, _qualification, _sector, _lastname, _firstname, _middlename, _extnname, _fullname, _contactnum, _email, _scholarship, _training, _assessment, 
-			_emp_before_training, _occupation, _employer, _emptype, _address, _datehired, _allocation);
-	INSERT INTO verifcation_records (verification_means, verification_date, verification_status, follow_up_date_1, response_status, not_interested_reason, referral_status) 
-    VALUES (_verifmeans, _verifdate, _verifstatus, _followupdate, _response, _notinterested, _refstatus);
-    INSERT INTO employment_records (company_name, company_address, job_title, employment_status, hired_date) 
-    VALUES (_companyname, _companyaddress, _jobtitle, _empstatus, _hireddate);
-END//
-DELIMITER ;
-*/
-
-/*
-DELIMITER //
-CREATE PROCEDURE exceldata_readall() 
-BEGIN
-	SELECT initial_records.Id, district, city, tvi, qualification_title, sector, last_name, first_name, middle_name, extension_name, full_name, contact_number, email, 
-			scholarship_type, training_status, assessment_result, employment_before_training, occupation, employer_name, employment_type, address, date_hired, allocation, 
-            verification_means, verification_date, verification_status, follow_up_date_1, response_status, not_interested_reason, referral_status, company_name, company_address, 
-            job_title, employment_status, hired_date 
-    FROM ((initial_records 
-    LEFT JOIN verification_records ON initial_records.Id = verification_records.Id) 
-    LEFT JOIN employment_records ON initial_records.Id = employment_records.Id);
-END//
-DELIMITER ;
-*/
-
-/*
-DELIMITER //
 CREATE PROCEDURE check_fullname(IN _last VARCHAR(255), IN _first VARCHAR(255), IN _middle VARCHAR(255), IN _extn CHAR(10))
 BEGIN
 	SELECT UPPER(last_name), UPPER(first_name), UPPER(middle_name), UPPER(extension_name) FROM initial_records 
@@ -108,6 +70,7 @@ BEGIN
 	TRUNCATE TABLE initial_records;
     TRUNCATE TABLE verification_records;
     TRUNCATE TABLE employment_records;
+    TRUNCATE TABLE external_records;
     SET FOREIGN_KEY_CHECKS = 1;
 END//
 DELIMITER ;
