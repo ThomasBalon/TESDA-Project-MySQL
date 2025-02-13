52163ab9293078698b64bec94d39a1cb3ea74601
Dave Andrew Solayao <102861448+code-solayao@users.noreply.github.com>
2025-02-13 16:56:44 +0800

    update 2025-02-13

--- tesda_project.sql
+++ tesda_project.sql
@@ -1,44 +1,71 @@
-/* CREATE TABLE IF NOT EXISTS initial_records (
-	Id INT AUTO_INCREMENT, 
-    district VARCHAR(50) NULL, 
-    city VARCHAR(50) NULL, 
-    tvi VARCHAR(255) NULL, 
-    qualification_title VARCHAR(255) NULL, 
-    sector VARCHAR(255) NULL, 
-    last_name VARCHAR(255) NOT NULL, 
-    first_name VARCHAR(255) NOT NULL, 
-    middle_name VARCHAR(255) NULL, 
-    extension_name VARCHAR(50) NULL, 
-    full_name VARCHAR(255) NULL, 
-    sex VARCHAR(50) NULL, 
-    birthdate VARCHAR(50) NULL, 
-    contact_number VARCHAR(16) NULL, 
-    email VARCHAR(255) NULL, 
-    scholarship_type VARCHAR(50) NULL, 
-    address VARCHAR(255) NULL, 
-    allocation VARCHAR(50) NULL, 
-    PRIMARY KEY(Id)
-);
+/* CREATE TABLE `graduates` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `district` varchar(50) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `tvi` varchar(255) DEFAULT NULL,
+  `qualification_title` varchar(255) DEFAULT NULL,
+  `sector` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `middle_name` varchar(255) DEFAULT NULL,
+  `extension_name` varchar(50) DEFAULT NULL,
+  `full_name` varchar(255) DEFAULT NULL,
+  `sex` varchar(50) DEFAULT NULL,
+  `birthdate` varchar(50) DEFAULT NULL,
+  `contact_number` varchar(16) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `scholarship_type` varchar(50) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `allocation` varchar(50) DEFAULT NULL,
+  `verification_means` varchar(50) DEFAULT NULL,
+  `verification_date` varchar(50) DEFAULT NULL,
+  `verification_status` varchar(50) DEFAULT NULL,
+  `follow_up_date_1` varchar(50) DEFAULT NULL,
+  `follow_up_date_2` varchar(50) DEFAULT NULL,
+  `response_status` varchar(50) DEFAULT NULL,
+  `not_interested_reason` varchar(255) DEFAULT NULL,
+  `referral_status` char(10) DEFAULT NULL,
+  `referral_date` varchar(50) DEFAULT NULL,
+  `no_referral_reason` varchar(255) DEFAULT NULL,
+  `invalid_contact` char(10) DEFAULT NULL,
+  `company_name` varchar(255) DEFAULT NULL,
+  `company_address` varchar(255) DEFAULT NULL,
+  `job_title` varchar(255) DEFAULT NULL,
+  `employment_status` varchar(255) DEFAULT NULL,
+  `hired_date` varchar(50) DEFAULT NULL,
+  `submitted_documents_date` varchar(50) DEFAULT NULL,
+  `interview_date` varchar(50) DEFAULT NULL,
+  `not_hired_reason` varchar(50) DEFAULT NULL,
+  `training_status` varchar(50) DEFAULT NULL,
+  `assessment_result` varchar(255) DEFAULT NULL,
+  `employment_before_training` varchar(50) DEFAULT NULL,
+  `occupation` varchar(255) DEFAULT NULL,
+  `employer_name` varchar(255) DEFAULT NULL,
+  `employment_type` varchar(255) DEFAULT NULL,
+  `date_hired` varchar(50) DEFAULT NULL,
+  `remarks` varchar(255) DEFAULT NULL,
+  `count` char(10) DEFAULT NULL,
+  `no_of_graduates` char(10) DEFAULT NULL,
+  `no_of_employed` char(10) DEFAULT NULL,
+  `verification` varchar(50) DEFAULT NULL,
+  `job_vacancies` char(10) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 */
 
 -- CALL read_all_records();
 -- CALL clear_all_records();
 -- SET AUTOCOMMIT = ON;
--- INSERT INTO initial_records (last_name, first_name, middle_name, full_name) VALUE ("Solayao", "Dave Andrew", "Alano", "Solayao, Dave Andrew Alano");
+-- INSERT INTO graduates (last_name, first_name, middle_name, extension_name, full_name) VALUE ("Dela Cruz", "Juan", "Antonio", "Sr.", "Dela Cruz Sr., Juan Antonio");
 
 /*
-RENAME TABLE scholarship_employment TO initial_records;
-ALTER TABLE initial_records ADD verification_status VARCHAR(50);
-ALTER TABLE initial_records ADD contact_number INT AFTER birthdate;
-ALTER TABLE initial_records DROP COLUMN verification_status;
-ALTER TABLE initial_records CHANGE middle_initial middle_name VARCHAR(50);
-ALTER TABLE initial_records MODIFY COLUMN qualification VARCHAR(50);
-*/
-
-/*
-DELETE FROM initial_records WHERE id = 1;
-DELETE FROM verification_records WHERE id = 1;
-DELETE FROM employment_records WHERE id = 1;
+RENAME TABLE scholarship_employment TO graduates;
+ALTER TABLE graduates ADD verification_status VARCHAR(50);
+ALTER TABLE graduates ADD contact_number INT AFTER birthdate;
+ALTER TABLE graduates DROP COLUMN verification_status;
+ALTER TABLE graduates CHANGE middle_initial middle_name VARCHAR(50);
+ALTER TABLE graduates MODIFY COLUMN qualification VARCHAR(50);
+DELETE FROM graduates WHERE id = 1;
 */
 
 /*
@@ -47,6 +74,21 @@ CREATE PROCEDURE `check_fullname`(IN _name VARCHAR(255))
 BEGIN
 	SELECT full_name FROM graduates 
     WHERE full_name = _name;
+END//
+DELIMITER ;
+*/
+
+/*
+DELIMITER //
+CREATE PROCEDURE `create_entry`(IN _district VARCHAR(50), IN _city VARCHAR(50), IN _tvi VARCHAR(255), IN _qualification VARCHAR(255), IN _sector VARCHAR(255), 
+									IN _lastname VARCHAR(255), IN _firstname VARCHAR(255), IN _middlename VARCHAR(255), IN _extname VARCHAR(50), IN _fullname VARCHAR(255), 
+                                    IN _sex VARCHAR(50), IN _birthdate VARCHAR(50), IN _contactnum VARCHAR(16), IN _email VARCHAR(255), IN _scholarship VARCHAR(50), 
+									IN _address VARCHAR(255), IN _allocation VARCHAR(50))
+BEGIN
+	INSERT INTO graduates (district, city, tvi, qualification_title, sector, last_name, first_name, middle_name, extension_name, full_name, 
+							sex, birthdate, contact_number, email, scholarship_type, address, allocation) 
+    VALUES (_district, _city, _tvi, _qualification, _sector, _lastname, _firstname, _middlename, _extname, _fullname, 
+			_sex, _birthdate, _contactnum, _email, _scholarship, _address, _allocation);
 END//
 DELIMITER ;
 */
@@ -83,16 +125,15 @@ DELIMITER ;
 
 /*
 DELIMITER //
-CREATE PROCEDURE read_all_records()
+CREATE PROCEDURE read_details(IN _id INT)
 BEGIN
-	SELECT initial_records.Id, district, city, tvi, qualification_title, sector, last_name, first_name, middle_name, extension_name, full_name, 
-			contact_number, email, scholarship_type, training_status, assessment_result, employment_before_training, occupation, employer_name, 
-            employment_type, address, date_hired, allocation, verification_means, verification_date, verification_status, follow_up_date_1, 
-            response_status, not_interested_reason, referral_status, company_name, company_address, job_title, employment_status, hired_date, remarks, 
-            count, no_of_graduates, no_of_employed, verification, job_vacancies 
-    FROM ((initial_records LEFT JOIN verification_records ON initial_records.Id = verification_records.Id) 
-    LEFT JOIN employment_records ON initial_records.Id = employment_records.Id) 
-    LEFT JOIN external_records ON initial_records.Id = external_records.Id;
+	SELECT district, city, tvi, qualification_title, sector, last_name, first_name, middle_name, extension_name, full_name, 
+			sex, birthdate, contact_number, email, scholarship_type, address, allocation, verification_means, verification_date, verification_status, 
+			follow_up_date_1, follow_up_date_2, response_status, not_interested_reason, referral_status, referral_date, no_referral_reason, invalid_contact, company_name, company_address, 
+            job_title, employment_status, hired_date, submitted_documents_date, interview_date, not_hired_reason, training_status, assessment_result, employment_before_training, occupation, 
+            employer_name, employment_type, date_hired, remarks, count, no_of_graduates, no_of_employed, verification, job_vacancies 
+    FROM graduates 
+    WHERE Id = _id;
 END//
 DELIMITER ;
 */
@@ -212,22 +253,6 @@ BEGIN
     FROM initial_records 
 	LEFT JOIN employment_records ON initial_records.Id = employment_records.Id 
     WHERE qualification_title = _title;
-END//
-DELIMITER ;
-*/
-
-/*
-DELIMITER //
-CREATE PROCEDURE PROCEDURE `submit_record_data`(IN _district VARCHAR(50), IN _city VARCHAR(50), IN _tvi VARCHAR(255), 
-								IN _qualification VARCHAR(255), IN _sector VARCHAR(255), IN _lastname VARCHAR(255), IN _firstname VARCHAR(255), 
-                                IN _middlename VARCHAR(255), IN _extname VARCHAR(50), IN _fullname VARCHAR(255), IN _sex VARCHAR(50), 
-                                IN _birthdate VARCHAR(50), IN _contactnum VARCHAR(16), IN _email VARCHAR(255), IN _scholarship VARCHAR(50), 
-                                IN _address VARCHAR(255), IN _allocation VARCHAR(50))
-BEGIN
-	INSERT INTO initial_records (district, city, tvi, qualification_title, sector, last_name, first_name, middle_name, extension_name, full_name, sex, 
-								birthdate, contact_number, email, scholarship_type, address, allocation) 
-    VALUES (_district, _city, _tvi, _qualification, _sector, _lastname, _firstname, _middlename, _extname, _fullname, _sex, _birthdate, _contactnum, 
-			_email, _scholarship, _address, _allocation);
 END//
 DELIMITER ;
 */
