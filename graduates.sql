6cff41188f7b2ea36e64b29be8efc4a7385bea79
Dave Andrew Solayao <solayao.daveandrew@gmail.com>
2025-03-12 16:42:29 +0800

    update 2025-03-12

--- graduates.sql
+++ graduates.sql
@@ -78,6 +78,46 @@ DELIMITER ;
 
 /*
 DELIMITER //
+CREATE PROCEDURE `import_records`(IN _created TIMESTAMP, IN _updated TIMESTAMP, IN _district VARCHAR(50), IN _city VARCHAR(50), IN _tvi VARCHAR(255), 
+									IN _qualification VARCHAR(255), IN _sector VARCHAR(255), IN _lastname VARCHAR(255), IN _firstname VARCHAR(255), IN _middlename VARCHAR(255), 
+                                    IN _extname VARCHAR(50), IN _fullname VARCHAR(255), IN _sex VARCHAR(50), IN _birthdate VARCHAR(50), IN _contactnum VARCHAR(16), 
+                                    IN _email VARCHAR(255), IN _address VARCHAR(255), IN _scholarship VARCHAR(50), IN _training VARCHAR(255), IN _assessment VARCHAR(255), 
+                                    IN _emp_before_training VARCHAR(255), IN _occupation VARCHAR(255), IN _employer VARCHAR(255), IN _emp_type VARCHAR(255), IN _work_address VARCHAR(255), 
+                                    IN _date_hired VARCHAR(255), IN _allocation VARCHAR(50), IN _verify_means VARCHAR(50), IN _verify_date VARCHAR(50), IN _verify_status VARCHAR(50), 
+                                    IN _followup_1 VARCHAR(50), IN _followup_2 VARCHAR(50), IN _response VARCHAR(50), IN _not_interested VARCHAR(255), IN _referral_status CHAR(10), 
+                                    IN _referral_date VARCHAR(50), IN _no_referral VARCHAR(255), IN _invalid_contact CHAR(10), IN _company_name VARCHAR(255), IN _company_address VARCHAR(255), 
+                                    IN _job_title VARCHAR(255), IN _application VARCHAR(255), IN _withdrawn VARCHAR(255), IN _emp_status VARCHAR(255), IN _hired VARCHAR(50), 
+                                    IN _submit_docs VARCHAR(50), IN _interview VARCHAR(50), IN _not_hired VARCHAR(50), IN _count CHAR(10), IN _graduates CHAR(10), 
+                                    IN _employed CHAR(10), IN _verification VARCHAR(50), IN _job_vacancies CHAR(10))
+BEGIN
+	INSERT INTO graduates (created_at, updated_at, district, city, tvi, 
+							qualification_title, sector, last_name, first_name, middle_name, 
+                            extension_name, full_name, sex, birthdate, contact_number, 
+                            email, address, scholarship_type, training_status, assessment_result, 
+                            employment_before_training, occupation, employer_name, employment_type, work_address, 
+                            date_hired, allocation, verification_means, verification_date, verification_status, 
+                            follow_up_date_1, follow_up_date_2, response_status, not_interested_reason, referral_status, 
+                            referral_date, no_referral_reason, invalid_contact, company_name, company_address, 
+                            job_title, application_status, withdrawn_reason, employment_status, hired_date, 
+                            submitted_documents_date, interview_date, not_hired_reason, count, no_of_graduates, 
+                            no_of_employed, verification, job_vacancies) 
+    VALUES (_created, _updated, _district, _city, _tvi, 
+			_qualification, _sector, _lastname, _firstname, _middlename, 
+			_extname, _fullname, _sex, _birthdate, _contactnum, 
+			_email, _address, _scholarship, _training, _assessment, 
+			_emp_before_training, _occupation, _employer, _emp_type, _work_address, 
+			_date_hired, _allocation, _verify_means, _verify_date, _verify_status, 
+			_followup_1, _followup_2, _response, _not_interested, _referral_status, 
+			_referral_date, _no_referral, _invalid_contact, _company_name, _company_address, 
+			_job_title, _application, _withdrawn, _emp_status, _hired, 
+			_submit_docs, _interview, _not_hired, _count, _graduates, 
+			_employed, _verification, _job_vacancies);
+END//
+DELIMITER ;
+*/
+
+/*
+DELIMITER //
 CREATE PROCEDURE login_user(IN _username VARCHAR(255))
 BEGIN
 	SELECT id, password, created 
